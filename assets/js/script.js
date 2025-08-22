cf9b55e16dab780f178c8471ff7365c1b9575199
navarrosuperthi <tsuperthi@gmail.com>
2025-08-22 12:11:51 -0300

    Criamos os eventos nos botões de login e register

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,28 @@
 const btnLogin = document.querySelector("#btn-login");
 const btnRegister = document.querySelector("#btn-register");
+const modalLogin = document.querySelector("#modal-login");
+const ModalRegister = document.querySelector("#modal-register");
+const btnCancelLogin = document.querySelector("#btn-cancel-login");
+const btnCancelRegister  = document.querySelector("#btn-cancel-register");
 
-btnLogin.addEventListener("click",(e) => {
-  console.log("cliquei no botão login");
-    
+btnLogin.addEventListener("click", (e) => {
+  modalLogin.classList.remove("hide-modal");
+  modalLogin.classList.add("show-modal");
+  
+})
+
+btnRegister.addEventListener("click", (e) => {
+  ModalRegister.classList.remove("hide-modal");
+  ModalRegister.classList.add("show-modal");
+})
+
+btnCancelLogin.addEventListener("click",(e) =>{
+  modalLogin.classList.add("hide-modal");
+  modalLogin.classList.remove("show-modal");
+})
+
+btnCancelRegister.addEventListener("click", (e) =>{
+  ModalRegister.classList.remove("show-modal");
+  ModalRegister.classList.add("hide-modal")
+  
 })
